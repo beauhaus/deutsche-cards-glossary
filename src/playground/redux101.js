a9b722e3225f27096c56eb0e4f32630db6b12229
BEAUHAUS <beau.html@gmail.com>
2019-02-15 16:09:04 -0500

    REDUX Dispatch exercise

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,10 +1,69 @@
 import { createStore } from 'redux';
 
+const store = createStore((state = { count: 0 }, action) => {
 
-const store = createStore((state = { count: 0 }) => {
-
-    return state;
+    switch (action.type) {
+        case "INCREMENT":
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+            return {
+                count: state.count + incrementBy
+            };
+        case "DECREMENT":
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
+            return {
+                count: state.count - decrementBy
+            };
+            case "RESET":
+            return {
+                count: 0
+            };
+            case "SET":
+            return {
+                count: action.count
+            };
+        default:
+            return state;
+    }
 });
 
-var store1 = store.getState();
-console.log(store1)
+
+store.dispatch({
+    type: "INCREMENT",
+});
+
+store.dispatch({
+    type: "DECREMENT",
+    decrementBy: 5
+});
+store.dispatch({
+    type: "INCREMENT",
+    incrementBy: 5
+});
+store.dispatch({
+    type: "RESET",
+});
+store.dispatch({
+    type: "INCREMENT",
+    incrementBy: 5
+});
+
+store.dispatch({
+    type: "SET",
+    count: 101
+});
+
+var storeLog = store.getState();
+console.table(storeLog)
+
+
+// store.dispatch({
+//     type: "DECREMENT"
+// });
+// var storeLog = store.getState();
+// console.table(storeLog)
+
+// store.dispatch({
+//     type: "RESET"
+// });
+// console.table(storeLog)
+
