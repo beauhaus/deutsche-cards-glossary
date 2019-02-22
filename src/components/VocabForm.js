d5e12a304ffc55c1c276880c9732b1ffe0a176f5
BEAUHAUS <beau.html@gmail.com>
2019-02-22 17:06:13 -0500

    cleanup

--- src/components/VocabForm.js
+++ src/components/VocabForm.js
@@ -4,36 +4,50 @@ import React, { Component } from 'react'
 class VocabForm extends Component {
     state = {
         word_de: '',
-        example_de: '',
         word_en: '',
+        example_de: '',
         example_en: '',
         note: '',
-        isShowing: true
+        isShowing: false
     }
 
     onWord_deChange = (e) => {
+        console.log("onWord_deChange ran")
         const word_de = e.target.value;
-        this.setState(() =>({word_de}))
-    }
-    onExample_deChange = (e) => {
-        const example_de = e.target.value;
-        this.setState(() =>({example_de}))
+        this.setState(() => ({ word_de }))
     }
     onWord_enChange = (e) => {
+        console.log("onWord_en Change ran")
         const word_en = e.target.value;
-        this.setState(() =>({word_en}))
+        this.setState(() => ({ word_en }))
     }
+
+    onExample_deChange = (e) => {
+        console.log("onExample_deChange ran")
+        const example_de = e.target.value;
+        this.setState(() => ({ example_de }))
+    }
+
     onExample_enChange = (e) => {
+        console.log("onExample_enChange ran")
         const example_en = e.target.value;
-        this.setState(() =>({example_en}))
+        this.setState(() => ({ example_en }))
     }
+
     onNoteChange = (e) => {
+        console.log("onNoteChange ran")
         const note = e.target.value;
-        this.setState(() =>({note}))
+        this.setState(() => ({ note }))
     }
+
     onIsShowingChange = (e) => {
+        console.log("isShowing ran")
+
         const isShowing = e.target.value;
-        this.setState(() =>({isShowing}))
+
+        this.setState((prevState) =>({
+            isShowing: !prevState.isShowing
+        }))
     }
 
     render() {
@@ -41,36 +55,51 @@ class VocabForm extends Component {
             <div>
                 <form>
                     <input
+                        onChange={this.onWord_deChange}
                         type="text"
-                        placeholder="word_de"
-                        autofocus
+                        placeholder="German Word"
+                        autoFocus
                         value={this.state.word_de}
                     />
                     <input
+                        onChange={this.onWord_enChange}
                         type="text"
-                        placeholder="word_en"
-                        autofocus
+                        placeholder="Eng Word"
                         value={this.state.word_en}
                     />
+                    <br />
                     <input
+                        onChange={this.onExample_deChange}
                         type="text"
-                        placeholder="example_de"
-                        autofocus
+                        placeholder="German Sentence"
                         value={this.state.example_de}
                     />
                     <input
+                        onChange={this.onExample_enChange}
                         type="text"
-                        placeholder="example_en"
-                        autofocus
+                        placeholder="English Sentence"
                         value={this.state.example_en}
                     />
                     <input
+                        onChange={this.onExample_enChange}
                         type="text"
-                        placeholder="amount"
+                        placeholder="English Sentence"
+                        value={this.state.example_en}
                     />
+
+                    <hr />
+                    <textarea placeholder="Add Note" onChange={this.onNoteChange}></textarea>
                     <br />
-                    <textarea placeholder="Add Note"></textarea>
-                    <br />
+                    <label>
+                    Is showing:
+                    <input
+                        name="isShowing"
+                        type="checkbox"
+                        value={this.state.isShowing}
+                        checked={this.state.isShowing}
+                        onChange={this.onIsShowingChange} />
+                    </label>
+                    <br/>
                     <button type="submit">Add VocabItem</button>
                 </form>
             </div>
