c6279ce64d79acc203140bcdea837146289edad3
BEAUHAUS <beau.html@gmail.com>
2019-02-21 22:08:12 -0500

    cleanup and sorting with select & options

--- src/components/VocabListFilters.js
+++ src/components/VocabListFilters.js
@@ -12,9 +12,16 @@ const VocabListFilters = (props) => (
                     props.dispatch(setTextFilter(e.target.value))
                 }}
         />
-        <select >
+        <select value={props.filters.sortBy}
+         onChange={(e) =>{
+             if (e.target.value === 'date') {
+                 props.dispatch(sortByDate())
+             } else if ( e.target.value === 'isShowing') {
+                 // TODO: not vital
+             }
+                  }}>
         <option value="date">date</option>
-        <option value="x">Date Created</option>
+        <option value="isShowing">Is Showing</option>
         </select>
     </div>
 );
