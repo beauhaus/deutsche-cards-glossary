7b0eca30ab54203b6fe6180168b6137659a8b0b7
BEAUHAUS <beau.html@gmail.com>
2019-03-11 00:52:54 -0400

    using jest --watch and passing 3 tests

--- src/app.js
+++ src/app.js
@@ -60,4 +60,3 @@ const JSX = () => (
     </Provider>
 )
 ReactDOM.render(<JSX/>, document.getElementById('app'));
-
--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,7 +1,8 @@
 const add = ((a, b ) => a + b);
+const greetMaker = ((name, greet ) => `${greet}, ${name}`);
 
 
-
+// yarn test -- --watch
 /***************** Assertion Library****************/
 /* 
 https://jestjs.io/docs/en/api
@@ -10,7 +11,13 @@ https://jestjs.io/docs/en/api
 
 /***************************************************/
 
-test('should add 2 numbers', () =>{
-    const result = add(3,4)
-    expect(result).toBe(7)
+// test('should add 2 numbers', () =>{
+//     const result = add(3,4)
+//     expect(result).toBe(7)
+// })
+
+test('should greet person by name', () =>{
+    const result = greetMaker("Billy","Good Morning")
+    expect(result).toBe("Good Morning, Billy")
 })
+
