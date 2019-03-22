97f90d33014172b06580f2ab5912ed576eda77af
BEAUHAUS <beau.html@gmail.com>
2019-03-22 11:58:37 -0400

    adds shallowrender test and snapshot for Routes

--- src/tests/Routing/Routes.test.js
+++ src/tests/Routing/Routes.test.js
@@ -1,2 +1,15 @@
 //react-test-renderer
-import renderer from 'react-test-renderer'
+import React from 'react';
+import ReactShallowRenderer from 'react-test-renderer/shallow';
+import Routes from '../../Routing/Routes'
+
+test('should render routes correctly',() => {
+    const renderer = new ReactShallowRenderer;
+
+    renderer.render(<Routes/>)
+   
+
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
+
+    console.log("R: \n", renderer.getRenderOutput())
+})
