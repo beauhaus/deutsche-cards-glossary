a26b4e4379a5a19bdf9b4463386095410cdc088d
BEAUHAUS <beau.html@gmail.com>
2019-03-28 19:19:22 -0400

    adds MainNav.test snapshot (passing)

--- src/tests/Routing/MainNav.test.js
+++ src/tests/Routing/MainNav.test.js
@@ -1,13 +1,13 @@
 //react-test-renderer
 import React from 'react';
 import {shallow} from 'enzyme';
-import Routes from '../../Routing/Routes'
+import MainNav from '../../Routing/MainNav'
 
 /****** ENZYME METHODS FOR SHALLOW RENDER: ******
 https://airbnb.io/enzyme/docs/api/shallow.html
 ************************************************/
 
 test('should render routes correctly',() => {
-    const wrapper = shallow(<Routes/>)
+    const wrapper = shallow(<MainNav/>)
     expect(wrapper).toMatchSnapshot();
 })
