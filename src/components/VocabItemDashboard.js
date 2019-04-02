0efbfd0ee04764200d7161bade6962cfc7336c01
BEAUHAUS <beau.html@gmail.com>
2019-04-02 14:09:12 -0400

    cleanup

--- src/components/VocabItemDashboard.js
+++ src/components/VocabItemDashboard.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import VocabItemList from './VocabItemList';
-import VocabListFilters from './VocabListfilters'
+import VocabListFilters from './VocabListfilters';
 
 const Styled_Template = styled.div`
-     width: 100vw;
+    width: 100vw;
     height: 100vh;
     grid-column: 1;
     grid-row: 2/-1;
