27bcbac676277c38d1c1eed76a7de96a00cfc77f
BEAUHAUS <beau.html@gmail.com>
2019-04-05 10:36:21 -0400

    cleanup prod build&server

--- server/server.js
+++ server/server.js
@@ -1,20 +1,16 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-
 const publicPath = path.join(__dirname, '..', 'public');
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-
 /* This will enable hot-linking so that router works properly (requests to sub-pages)...*/
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 })
-
-const Local_PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Express Server is up @ PORT ${PORT}`)
