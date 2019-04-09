7042fbcd3acbdab17f16e60a85637bed3fd16b0c
BEAUHAUS <beau.html@gmail.com>
2019-04-08 21:16:20 -0400

    db adjustments

--- src/app.js
+++ src/app.js
@@ -13,42 +13,14 @@ import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 import Routes from './Routing/Routes';
+import db from './data/wordsDB.JSON';
 
+console.log("db: ", db)
 // setLS();
 
 const store = configureStore();
 
-const vocabItemOne = store.dispatch(addVocabItem({
-    word_de: "glücklich",
-    word_en: "happy",
-    example_de: 'Ich bin glücklich',
-    example_en: 'I am happy',
-    note: 'happy note',
-    difficulty: 10,
-    createdAt: 33333320000,
-    isShowing: false
-}))
-const vocabItemTwo = store.dispatch(addVocabItem({
-    word_de: "müde",
-    word_en: "tired",
-    example_de: 'du bist müde',
-    example_en: 'you are tired',
-    note: 'tired note',
-    difficulty: 3,
-    createdAt: 100,
-    isShowing: true
-}))
-const vocabItemThree = store.dispatch(addVocabItem({
-    word_de: "verstärken",
-    word_en: "strengthen",
-    example_de: 'Wir müssen die Wände verstärken.',
-    example_en: 'We must strengthen the walls',
-    note: 'strengthen note',
-    difficulty: 8,
-    createdAt: 99995552000,
-    isShowing: false
-}))
-
+db.map(item => store.dispatch(addVocabItem(item)))
 
 const state = store.getState();
 const visibleVocabItems = getVisibleVocabItems(state.vocabItems, state.filters);
